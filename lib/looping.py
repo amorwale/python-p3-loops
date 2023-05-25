f968f1ea3ed419c2fb0f3601f1d383efd7a080df
Ashay Morwale <ashay123.m@gmail.com>
2023-05-25 15:06:16 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    x = 10
+    while x > 0:
+        print(x)
+        x -= 1
+    print('Happy New Year!')
     pass
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
+    
 
 def fizzbuzz():
-    # code goes here!
+    x = 1
+    while x <= 100:
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz") 
+        elif x % 3 == 0:
+            print("Fizz") 
+        elif x % 5 == 0:
+            print("Buzz") 
+        else: print(x) 
+        x += 1
     pass
